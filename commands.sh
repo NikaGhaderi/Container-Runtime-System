3e3fdea44d849ffb0251d061f300117089dab3d7
NikaGhaderi <nika.ghadery@gmail.com>
2025-06-14 18:35:05 -0400

    prep for step 3

--- commands.sh
+++ commands.sh
@@ -8,5 +8,5 @@ pstree -p | grep my_runner
 
 # In Terminal 2, use the HOST PID of the container
 # In our example, the PID of the sleep process is 34567
-sudo cat /proc/34567/mountinfo
+sudo cat /proc/7207/mountinfo
 
